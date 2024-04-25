73ca9ed89db7843c3cb432e2460dfc8e589cb35a
waleedsy <waleedsani2013@gmail.com>
2024-04-25 17:35:59 +0100

    auth

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,56 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useState } from 'react'
+import { images } from '../../constants'
+import FormField from '../../components/FormField'
 
 const signIn = () => {
+
+    const [form, setForm] = useState(
+        {
+            email: '',
+            password: ''
+        }
+    )
+
   return (
-    <View>
-      <Text>Signin</Text>
-    </View>
+      <SafeAreaView className="bg-primary h-full">
+          <ScrollView>
+              {/* min-h-[85vh]*/}
+              <View className="w-full justify-center h-full px-4 my-6">
+                  <Image source={images.logo}
+                      resizeMode='contain'
+                      className="w-[115px] h-[35px]"
+                  />
+                  <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+                  <FormField
+                      title="Email"
+                      value={form.email}
+                      handleChangeText={(e) => 
+                          setForm({
+                              ...form,
+                              email: e
+                          }
+                          )}
+                      otherStyles="mt-7"
+                      keyboardType="email-address"
+                      
+                  />
+                    <FormField
+                      title="Password"
+                      value={form.password}
+                      handleChangeText={(e) => 
+                          setForm({
+                              ...form,
+                              password: e
+                          }
+                          )}
+                      otherStyles="mt-7"
+                  />
+              </View>
+          </ScrollView>
+     </SafeAreaView>
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
-import { Link } from "expo-router"
+import { Link, Redirect, router } from "expo-router"
 import { NativeWindStyleSheet } from "nativewind";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -14,7 +15,7 @@ export default function App() {
     <>
       <SafeAreaView className="bg-primary h-full">
         <ScrollView contentContainerStyle={{ height: '100%' }}>
-          <View className="w-full justify-center items-center h-full px-4">
+          <View className="w-full justify-center items-center min-h-[85vh] px-4">
             <Image source={images.logo}
               className="w-[130px] h-[84px]"
               resizeMode='contain'
@@ -28,11 +29,21 @@ export default function App() {
             <Text className="text-secondary-200">Aora</Text>
               </Text>
               <Image source={images.path}
-              className="w-[136] h-[15px] absolute-bottom-2 -right-8"
+                className="w-[136] h-[15px] absolute -bottom-2 -right-8"
+                resizeMode='contain'
               />
-          </View>
+            </View> 
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+              Where creativity meets innovation: embark on a journey of limitless exploration with Aora 
+            </Text>
+            <CustomButton
+              title="Continue with Email"
+              handlePress={() => {router.push('/sign-in') }}
+              containerStyles="w-full mt-7"
+            />
           </View>
         </ScrollView>
+        <StatusBar backgroundColor='#161622' style='light' />
       </SafeAreaView>
       </>
   );
